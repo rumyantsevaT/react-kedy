e280fd1919395ee5a5f65d71812f9a8c7817f72b
Tatiana Rumyantseva <tinarum@MBP-Tatiana.Dlink>
2022-02-18 20:05:42 +0300

    косяк с macro-css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
-
-
 function App() {
   return (
-    <div className="App">
-      я Румянцева Татьяна зарабатываю пассивным доходом 750 тысяч рублей в месяц
-      ежемесячно!!! да будет так!!!! да
-
+    <div className="wrapper">
+      <header className="d-flex justify-between">
+        <div className="headerLeft">
+          <img width="40" height="40" src="/img/logo.png" alt="" />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+          <img width={18} height={18} src="/img/cart.svg" alt="" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+          <img width={18} height={18} src="/img/favorite.svg" alt="" />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+        ... карточки
+      </div>
     </div>
   );
 }
